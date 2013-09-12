126084b71e37d505aaa56698d4297287f3b1da60
Masahiro Sano <sabottenda@gmail.com>
2013-09-12 21:02:23 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,66 @@
+# fluent-plugin-stdout-pp
+
+A fluentd plugin to pretty print json with color to stdout
+
+## Installation
+
+Add this line to your application's Gemfile:
+
+    gem 'fluent-plugin-stdout-pp'
+
+And then execute:
+
+    $ bundle
+
+Or install it yourself as:
+
+    $ gem install fluent-plugin-stdout-pp
+
+## Configuration
+
+```
+<match **>
+  type stdout_pp
+  time_color blue
+  tag_color yellow
+</match>
+```
+
+## Parameters
+
+|parameter|description|default|
+|---|---|---|
+|pp| prety print json or not | true |
+|time_color| color name to print time | blue |
+|tag_color| color name to print tag | yellow |
+|record_colored| print json with color or not | true |
+
+Available colors:
+- normal
+- red
+- green
+- yellow
+- magenta
+- white
+
+## Contributing
+
+1. Fork it
+2. Create your feature branch (`git checkout -b my-new-feature`)
+3. Commit your changes (`git commit -am 'Add some feature'`)
+4. Push to the branch (`git push origin my-new-feature`)
+5. Create new Pull Request
+
+## Copyright
+
+<table>
+  <tr>
+    <td>Author</td><td>Masahiro Sano <sabottenda@gmail.com></td>
+  </tr>
+  <tr>
+    <td>Copyright</td><td>Copyright (c) 2013- Masahiro Sano</td>
+  </tr>
+  <tr>
+    <td>License</td><td>MIT License</td>
+  </tr>
+</table>
---
+++ lib/fluent/plugin/out-stdout-pp.rb
@@ -0,0 +1,57 @@
+require 'coderay'
+
+module Fluent
+  class StdoutPPOutput < Output
+    Fluent::Plugin.register_output('stdout_pp', self)
+
+    config_param :pp, :bool, :default => true
+    config_param :time_color, :string, :default => 'blue'
+    config_param :tag_color, :string, :default => 'yellow'
+    config_param :record_colored, :bool, :default => true
+
+    TTY_COLOR = {
+      normal:  "\033[0;39m",
+      red:     "\033[1;31m",
+      green:   "\033[1;32m",
+      yellow:  "\033[1;33m",
+      blue:    "\033[1;34m",
+      magenta: "\033[1;35m",
+      cyan:    "\033[1;36m",
+      white:   "\033[1;37m",
+    }
+
+    def initialize
+      super
+    end
+
+    def configure(conf)
+      super
+
+      @time_color = @time_color.intern
+      if TTY_COLOR[@time_color].nil?
+        raise ConfigError, "stdout_pp: unknown color name #{@time_color} in time_color"
+      end
+
+      @tag_color = @tag_color.intern
+      if TTY_COLOR[@tag_color].nil?
+        raise ConfigError, "stdout_pp: unknown color name #{@tag_color} in tag_color"
+      end
+    end
+
+    def emit(tag, es, chain)
+      tag_colored = TTY_COLOR[@tag_color] + tag + TTY_COLOR[:normal]
+      es.each do |time, record|
+        time_colored = TTY_COLOR[@time_color] + Time.at(time).localtime.to_s + TTY_COLOR[:normal]
+        if @pp
+          json = JSON.pretty_generate(record)
+        else
+          json = Yajl.dump(record)
+        end
+        json = CodeRay.scan(json, :json).terminal if @record_colored
+        $log.write "#{time_colored} #{tag_colored}: #{json}\n"
+      end
+      $log.flush
+      chain.next
+    end
+  end
+end
