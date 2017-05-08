38ed649371e726d87e34049e2e155bd295a141e9
Kenji Okimoto <okimoto@clear-code.com>
2017-05-08 17:10:37 +0900

    Remove unused method

--- test/helper.rb
+++ test/helper.rb
@@ -4,20 +4,3 @@ $LOAD_PATH.unshift(File.join(__dir__, '..', 'lib'))
 $LOAD_PATH.unshift(__dir__)
 require 'fluent/test'
 
-def capture(&block)
-  old = $log
-  $log = StringIO.new
-  yield
-  return $log.string
-ensure
-  $log = old
-end
-
-# def capture
-#   old = $stdout
-#   $stdout = StringIO.new('','w')
-#   yield
-#   $stdout.string
-# ensure
-#   $stdout = old
-# end
